09221de0f01a7da639f82753681a34bda10b879b
Hüseyin Hamad <huseyinhamad@hotmail.com>
2021-05-13 12:38:22 +0300

    Navbar oluşturulurken çıkan hatalar giderildi

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5', image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/38277b478a79419abf6da998000ab640_9366/Runfalcon_Shoes_Black_F36218_01_standard.jpg' },
@@ -9,8 +10,10 @@ const products = [
 ]
 
 const Products = () => {
+    const classes = useStyles();
     return(
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify = "center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key = {product.id} xs={12} sm={6} md={4} lg={3}>
