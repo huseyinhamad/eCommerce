c8a03adf69362c1bf7661a6e6942f09443893353
Hüseyin Hamad <huseyinhamad@hotmail.com>
2021-05-13 12:34:03 +0300

    Navbar eklendi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Products from './Components/Products/Products';
+
+import {Products, Navbar} from './Components';
 
 const App = () => {
     return (
         <div>
+            <Navbar />
             <Products />    
         </div>
     )
